9a77618c6190b067c0222b135718b41f66f7f7ce
Romario Negreiros <nromario482@gmail.com>
2022-08-21 00:18:01 -0300

    Add use env DB-NAME variable to complete the mongodb URI string in order to connect with the database

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const connect = async () => {
-  await mongoose.connect(`mongodb+srv://${process.env['DB-USERNAME']}:${process.env['DB-PASSWORD']}@cluster0.ytyyeyl.mongodb.net/github-followers-tracker`)
+  await mongoose.connect(
+    `mongodb+srv://${process.env['DB-USERNAME']}:${process.env['DB-PASSWORD']}@cluster0.ytyyeyl.mongodb.net/${process.env['DB-NAME']}`
+  )
 }
 
 export default connect
