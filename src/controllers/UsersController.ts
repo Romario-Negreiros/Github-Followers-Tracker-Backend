6d5884db3afa8a03896ee000e81b07a8e639ee6b
Romario Negreiros <nromario482@gmail.com>
2022-08-21 22:43:48 -0300

    Create UsersController class register and unregister methods
    Register method adds a new instance of the TrackedUser model to the database.
    Unregister method removes a TrackedUser from the database.

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,40 @@
+import TrackedUser from '../models/trackedUser'
+
+import type { Request, Response } from 'express'
+
+class UsersController {
+  async register (req: Request, res: Response) {
+    const { name, email } = req.body
+    if (!name || !email) {
+      return res.status(400).json({ error: 'Missing either name or email field!' })
+    }
+
+    if (await TrackedUser.findOne({ email }).exec()) {
+      return res.status(409).json({ error: 'User is already registered!' })
+    }
+
+    const trackedUser = new TrackedUser({ name, email })
+    try {
+      await trackedUser.save()
+      return res.status(201).send('User added to tracking list, you\'ll receive updates about your github profile every hour!')
+    } catch (err) {
+      return res.status(500).json({ error: 'Failed to register new tracked user!' })
+    }
+  }
+
+  async unregister (req: Request, res: Response) {
+    const { email } = req.body
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email field!' })
+    }
+
+    const user = await TrackedUser.findOneAndDelete({ email })
+    if (!user) {
+      return res.status(404).json({ error: 'This user is not registered!' })
+    }
+
+    return res.status(204).send('User succesfully unregistered!')
+  }
+}
+
+export default new UsersController()
