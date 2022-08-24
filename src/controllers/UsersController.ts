630c39a9caf74fa0bd8d45f57533b30d6e2b7675
Romario Negreiros <nromario482@gmail.com>
2022-08-23 23:01:04 -0300

    Update register method and add update method
    Create update method and set updatedAt field in user data.
    In register method, set createdAt property of user object using a instance of the Date class with Date.now as argument, and use toLocaleString to adapt the date to the users locale.

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -6,19 +6,28 @@ class UsersController {
   register = async (req: Request, res: Response) => {
     const { name, email } = req.body
     if (!name || !email) {
-      return res.status(400).json({ error: 'Missing either name or email field!' })
+      return res
+        .status(400)
+        .json({ error: 'Missing either name or email field!' })
     }
 
     if (await TrackedUser.findOne({ email }).exec()) {
       return res.status(409).json({ error: 'User is already registered!' })
     }
 
-    const trackedUser = new TrackedUser({ name, email })
+    const createdAt = new Date(Date.now()).toLocaleString()
+    const trackedUser = new TrackedUser({ name, email, createdAt })
     try {
       await trackedUser.save()
-      return res.status(201).send('User added to tracking list, you\'ll receive updates about your github profile every hour!')
+      return res
+        .status(201)
+        .send(
+          "User added to tracking list, you'll receive updates about your github profile every hour!"
+        )
     } catch (err) {
-      return res.status(500).json({ error: 'Failed to register new tracked user!' })
+      return res
+        .status(500)
+        .json({ error: 'Failed to register new tracked user!' })
     }
   }
 
@@ -33,7 +42,27 @@ class UsersController {
       return res.status(404).json({ error: 'This user is not registered!' })
     }
 
-    return res.status(204).send('User succesfully unregistered!')
+    return res.status(204).end()
+  }
+
+  update = async (req: Request, res: Response) => {
+    const { name, email, newEmail } = req.body
+    if (!name || !email) {
+      return res
+        .status(400)
+        .json({ error: 'Missing either name or email field!' })
+    }
+
+    const updatedAt = new Date(Date.now()).toLocaleString()
+    const user = await TrackedUser.findOneAndUpdate(
+      { email },
+      { name, email: newEmail || email, updatedAt }
+    )
+    if (!user) {
+      return res.status(404).json({ error: 'This user is not registered!' })
+    }
+
+    return res.status(204).end()
   }
 }
 
