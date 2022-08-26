a489c981ad0767aec61b686c07054c93a63f41a2
Romario Negreiros <nromario482@gmail.com>
2022-08-25 22:43:53 -0300

    Refactor GithubResponse file
    Change name to GithubAPIReponses.
    Rename GithubResponse interface to User.
    Create Follower and Following interfaces.

---
+++ src/TrackingBot/types/GithubAPIResponses.ts
@@ -0,0 +1,41 @@
+export interface User {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string | null
+  gravatar_id: string | null
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+  name: string
+  company: string | null
+  blog: string | null
+  location: string | null
+  email: string | null
+  hireable: boolean
+  bio: string | null
+  twitter_username: string | null
+  public_repos: number
+  public_gists: number
+  followers: number
+  following: number
+  created_at: Date
+  updated_at: Date
+}
+
+export interface Follower {
+  login: string;
+  avatar_url: string | null;
+}
+
+export interface Following extends Follower {}
