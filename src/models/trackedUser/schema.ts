e12951576e9134e84e793941bee7f008b3f06338
Romario Negreiros <nromario482@gmail.com>
2022-08-25 23:00:20 -0300

    Add followers and following properties types.

--- src/models/trackedUser/schema.ts
+++ src/models/trackedUser/schema.ts
@@ -5,6 +5,15 @@ const { Schema } = mongoose
 const trackedUserSchema = new Schema({
   name: String,
   email: String,
+  followers: [{
+    login: String,
+    avatar_url: { type: String, default: null }
+  }],
+  following: [{
+    login: String,
+    avatar_url: { type: String, default: null }
+  }],
+  checkedAt: { type: Date, default: null },
   createdAt: Date,
   updatedAt: { type: Date, default: null }
 })
