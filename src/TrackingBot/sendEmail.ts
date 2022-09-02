f88b6168bdfc69591b01692fd06944ab8eb7f407
Romario Negreiros <nromario482@gmail.com>
2022-09-02 13:02:33 -0300

    Use fs.readFile to get the user's report, then assign it to the content property converting to a base64 string and send the email.
    Not dinamically passing the user's email in the 'to' property of the msg object due to error

---
+++ src/TrackingBot/sendEmail.ts
@@ -0,0 +1,32 @@
+import sgMail from '@sendgrid/mail'
+import fs from 'fs'
+
+interface User {
+  name: string
+  email: string
+}
+
+sgMail.setApiKey(process.env['SENDGRID-API-KEY'] as string)
+
+const sendEmail = async (user: User) => {
+  fs.readFile(`${user.name}-report.pdf`, async (err, data) => {
+    if (err) {
+      throw err
+    }
+
+    const msg: sgMail.MailDataRequired = {
+      to: 'valeubeleza72@gmail.com',
+      from: 'github.followers.tracker@outlook.com',
+      subject: 'Daily report',
+      text: 'Here is your github-followers-tracker daily report about your profile followers list changes',
+      attachments: [{
+        filename: `${user.name}-report.pdf`,
+        content: data.toString('base64')
+      }]
+    }
+
+    await sgMail.send(msg)
+  })
+}
+
+export default sendEmail
