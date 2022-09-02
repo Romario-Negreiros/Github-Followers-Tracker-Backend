5bc9c355a1852f9f9e0924d03ccf6b4e5a4d6b2e
Romario Negreiros <nromario482@gmail.com>
2022-09-02 12:59:58 -0300

    Wrap PDF generation in a promise to make sure the file will be available when sending the email

--- src/TrackingBot/generatePDF.ts
+++ src/TrackingBot/generatePDF.ts
@@ -4,71 +4,73 @@ import fs from 'fs'
 import type { Follower } from './types/GithubAPIResponses'
 
 const generatePDF = async (userName: string, follows: Follower[], unfollows: Follower[]) => {
-  const doc = new PDFDocument({ size: 'A4' })
-  doc.font('Helvetica')
+  await new Promise(() => {
+    const doc = new PDFDocument({ size: 'A4' })
+    doc.font('Helvetica')
 
-  doc
-    .fontSize(30)
-    .text('Github followers tracker', {
-      align: 'center'
-    })
-    .moveDown()
+    doc
+      .fontSize(30)
+      .text('Github followers tracker', {
+        align: 'center'
+      })
+      .moveDown()
 
-  doc.fontSize(25).text('Daily report', { align: 'center' }).moveDown()
+    doc.fontSize(25).text('Daily report', { align: 'center' }).moveDown()
 
-  doc.fontSize(18).text('Follows', { underline: true }).moveDown()
+    doc.fontSize(18).text('Follows', { underline: true }).moveDown()
 
-  if (follows.length) {
-    follows.forEach(follow => {
-      doc
-        .fontSize(14)
-        .fillColor('black')
-        .text(
-          `> ${follow.login} - (${
-            follow.isYouFollowing ? 'You are following this profile' : 'You are not following this profile'
-          })`
-        )
-        .moveDown(0)
-      if (follow.isYouFollowing) {
-        doc.fontSize(12).fillColor('blue').text('VISIT PROFILE TO FOLLOW', {
-          link: follow.html_url,
-          underline: true
-        })
-      }
-      doc.moveDown()
-    })
-  } else {
-    doc.fontSize(14).text('No follows today!')
-  }
+    if (follows.length) {
+      follows.forEach(follow => {
+        doc
+          .fontSize(14)
+          .fillColor('black')
+          .text(
+            `> ${follow.login} - (${
+              follow.isYouFollowing ? 'You are following this profile' : 'You are not following this profile'
+            })`
+          )
+          .moveDown(0)
+        if (follow.isYouFollowing) {
+          doc.fontSize(12).fillColor('blue').text('VISIT PROFILE TO FOLLOW', {
+            link: follow.html_url,
+            underline: true
+          })
+        }
+        doc.moveDown()
+      })
+    } else {
+      doc.fontSize(14).text('No follows today!')
+    }
 
-  doc.moveDown()
-  doc.fontSize(18).fillColor('black').text('Unfollows', { underline: true }).moveDown()
+    doc.moveDown()
+    doc.fontSize(18).fillColor('black').text('Unfollows', { underline: true }).moveDown()
 
-  if (unfollows.length) {
-    unfollows.forEach(unfollow => {
-      doc
-        .fontSize(14)
-        .fillColor('black')
-        .text(
-          `> ${unfollow.login} - (${
-            unfollow.isYouFollowing ? 'You are following this profile' : 'You are not following this profile'
-          })`
-        )
-        .moveDown(0)
-      if (unfollow.isYouFollowing) {
-        doc.fontSize(12).fillColor('blue').text('VISIT PROFILE TO UNFOLLOW', {
-          link: unfollow.html_url,
-          underline: true
-        })
-      }
-      doc.moveDown()
-    })
-  } else {
-    doc.fontSize(14).text('No unfollows today!')
-  }
+    if (unfollows.length) {
+      unfollows.forEach(unfollow => {
+        doc
+          .fontSize(14)
+          .fillColor('black')
+          .text(
+            `> ${unfollow.login} - (${
+              unfollow.isYouFollowing ? 'You are following this profile' : 'You are not following this profile'
+            })`
+          )
+          .moveDown(0)
+        if (unfollow.isYouFollowing) {
+          doc.fontSize(12).fillColor('blue').text('VISIT PROFILE TO UNFOLLOW', {
+            link: unfollow.html_url,
+            underline: true
+          })
+        }
+        doc.moveDown()
+      })
+    } else {
+      doc.fontSize(14).text('No unfollows today!')
+    }
 
-  doc.pipe(fs.createWriteStream(`${userName}-report.pdf`))
-  doc.end()
+    doc.pipe(fs.createWriteStream(`${userName}-report.pdf`))
+    doc.end()
+  })
 }
 
 export default generatePDF
