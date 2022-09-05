4e5bc90a7b3ea49d3d469586ad02dddd739526f1
Romario Negreiros <nromario482@gmail.com>
2022-09-04 22:25:39 -0300

    Refactor and add new methods
    Make bots variable private and delete emitter variable.
    Add methods to create a async task, a simple interval job, and use them all inside addSchedule method.
    Add removeBot method to exclude a bot from bots list.

--- src/service/classes/Scheduler.ts
+++ src/service/classes/Scheduler.ts
@@ -1,14 +1,50 @@
-import EventEmitter from 'events'
-import { ToadScheduler } from 'toad-scheduler'
+import { ToadScheduler, SimpleIntervalJob, AsyncTask } from 'toad-scheduler'
 import TrackingBot from './TrackingBot'
+import TrackedUser from '../../models/trackedUser'
 
 class Scheduler extends ToadScheduler {
-  bots: TrackingBot[] = []
-  emitter = new EventEmitter()
+  private bots: TrackingBot[] = []
 
-  constructor () {
-    super()
-    console.log('oi')
+  addScheduleForSavedUsers = async () => {
+    const users = await TrackedUser.find({})
+    for (const user of users) {
+      const bot = new TrackingBot(user.name as string, user.email as string)
+      if (bot.userNotFound) {
+        await TrackedUser.deleteOne({ email: user.email })
+      } else {
+        this.addSchedule(bot)
+      }
+    }
+  }
+
+  addSchedule = async (bot: TrackingBot) => {
+    const asyncTask = this.createAsyncTask(bot)
+    const job = this.createJob(asyncTask, bot.getUserEmail())
+    this.addSimpleIntervalJob(job)
+    this.bots.push(bot)
+  }
+
+  createAsyncTask = (bot: TrackingBot) => {
+    return new AsyncTask(
+      `Track ${bot.getUserName()}'s followers list changes`,
+      () => bot.checkGithubProfile()
+    )
+  }
+
+  createJob = (task: AsyncTask, id: string) => {
+    return new SimpleIntervalJob({ seconds: 15 }, task, id)
+  }
+
+  removeJob = (id: string) => this.removeById(id)
+
+  removeBot = (email: string) => {
+    this.bots = this.bots.filter(bot => {
+      if (bot.getUserEmail() === email) {
+        return null
+      } else {
+        return bot
+      }
+    })
   }
 }
 
